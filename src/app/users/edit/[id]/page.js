d55c320244ef971e2f5dc41fa587990414854bd6
Hxruyuk1 <nack91251@gmail.com>
2024-08-16 10:59:41 +0700

    ...

--- src/app/users/edit/[id]/page.js
+++ src/app/users/edit/[id]/page.js
@@ -43,7 +43,7 @@ export default function Page({params}) {
   useEffect(() => {
     async function getUsers() {
       try {
-        const res = await fetch(`https://backend-six-teal.vercel.app/api/users/${id}`);
+        const res = await fetch(`https://backend-six-teal.vercel.app/api/user/edit/${id}`);
         if (!res.ok) {
           console.error('Failed to fetch data');
           return;
